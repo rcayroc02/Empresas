7cf48a0c7a7ac8aa2ad561e085d6aaea402b4bb6
Gabriel <vgabriel.gm37@gmail.com>
2024-05-19 11:41:49 -0500

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'p_empresas_menu',
+  webDir: 'dist'
+};
+
+export default config;
