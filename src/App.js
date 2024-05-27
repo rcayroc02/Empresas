358883bf7f0d6df49ba9d319ec4a1a1b80f81997
rcayroc02 <81270845+rcayroc02@users.noreply.github.com>
2024-05-27 03:30:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Log_in from './Home';
 import Page2 from './Page2';
+import Service from './Service';  // Importa el nuevo componente
 
 function App() {
   return (
@@ -10,15 +11,19 @@ function App() {
         <nav style={styles.navbar}>
           <Link to="/" style={styles.link}>Log In</Link>
           <Link to="/page2" style={styles.link}>Sign In</Link>
+          <Link to="/service" style={styles.link}>Service</Link>  {/* Añade el nuevo enlace */}
         </nav>
         <Routes>
           <Route path="/" element={<Log_in />} />
           <Route path="/page2" element={<Page2 />} />
+          <Route path="/service" element={<Service />} />  {/* Añade la nueva ruta */}
         </Routes>
       </div>
     </Router>
   );
+  
 }
+
 
 const styles = {
   container: {
